d9528885ef68d4dbd2d0504e26f85036cd4bfd43
shibin <shibin.blesson@scrapeher.com>
2024-05-11 21:01:41 +0530

    add new files and other changes

--- new_feature.py
+++ new_feature.py
@@ -1,1 +1,15 @@
 "Print('New feature')" 
+#write a program that prints out the string "New feature"
+#rewrite the program below to add two numbers and execute the program
+#add two numbers    
+print(1+2)
+print('New feature')
+#write a program that prints out the string "New feature"
+
+# Path: new_feature.py
+"print('New feature')"
+#write a program that prints out the string "New feature"
+#rewrite the program below to add two numbers and execute the program
+
+#add two numbers
+print(1+2)
